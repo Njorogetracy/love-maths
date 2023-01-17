8a70a5a5a87a754f6f2ee65c8da8ff4e12093206
Hope Tracy <hnjoroge64@gmail.com>
2023-01-17 17:06:59 +0000

    Add subtraction code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
      } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+     } else if (gameType === "subtract") {
+        dispalySubtractQuestion(num1, num2);
      } else {
         alert(`unknown gmae type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`
@@ -73,6 +75,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimlemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`
@@ -106,7 +110,11 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function dispalySubtractQuestion() {
+function dispalySubtractQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent =  operand2 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
