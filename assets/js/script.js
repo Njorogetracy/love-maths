5c01c822f5274e4f03ccfea5456ed35d380d6785
Hope Tracy <hnjoroge64@gmail.com>
2023-01-17 14:57:51 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit!")
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
+
 })
 
 
@@ -20,9 +23,16 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
      let num1 = Math.floor(Math.random() * 25) + 1;
      let num2 = Math.floor(Math.random() * 25) + 1;
+
+     if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+     } else {
+        alert(`unknown gmae type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`
+     }
 }
 
 
@@ -42,8 +52,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function dispalySubtractQuestion() {
